5fb637a2bf1fc6a11bba832dba36a0ca9fd0803c
MiloDou <158305280+MiloDou@users.noreply.github.com>
2024-08-07 09:09:37 -0600

    Creación de las clases

---
+++ Laboratorio 2/Habitacion simple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_2
+{
+    public class Habitacion_simple
+    {
+
+
+    }
+}
---
+++ Laboratorio 2/Manejo de Habitaciones.cs
@@ -0,0 +1,15 @@
+﻿using Laboratorio_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_2
+{
+    public class Manejo_de_Habitaciones
+    {
+    }
+  
+}
+
